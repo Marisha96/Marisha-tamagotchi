96a4f8abfd06d73861956846f5327338a5903750
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-02 12:57:23 -0500

    added images

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ const areYouLives = 7;
 
 //link text 
 areYouSmart.textContent = areYouLives;
+
+// Generarting data
+// [{ imgSrc: './African American fLAG.gif'}]
+// images.src = imgscr
+
+const getData = () => [
+{ imgSrc: "./African American fLAG.gif", name: "African American fLAG"},
+{imgSrc: "./Barbados.gif", name: "Barbados"},
+{imgSrc: "./ethiopia.gif", name: "ethiopia"},
+{imgSrc: "./Ghana.gif", name: "Ghana"},
+{imgSrc: "./Guyana.gif", name: "Guyana"},
+{imgSrc: "./Haiti.gif", name: "Haiti"},
+{imgSrc: "./Jmaiaca.gif", name: "Jamaica"},
+{imgSrc: "./Nigeria.gif", name: "Nigeria"},
+{imgSrc: "./South-Africa.gif", name: "South-Africa"},
+{imgSrc: "./Vincy.gif", name: "Vincy"},
+{ imgSrc: "./African American fLAG.gif", name: "African American fLAG"},
+{imgSrc: "./Barbados.gif", name: "Barbados"},
+{imgSrc: "./ethiopia.gif", name: "ethiopia"},
+{imgSrc: "./Ghana.gif", name: "Ghana"},
+{imgSrc: "./Guyana.gif", name: "Guyana"},
+{imgSrc: "./Haiti.gif", name: "Haiti"},
+{imgSrc: "./Jmaiaca.gif", name: "Jamaica"},
+{imgSrc: "./Nigeria.gif", name: "Nigeria"},
+{imgSrc: "./South-Africa.gif", name: "South-Africa"},
+{imgSrc: "./Vincy.gif", name: "Vincy"},
+];
+const data = getData();
