bda7d2b5d77771725d6b3e62dc16efc697afeb45
Marisha96 <Marisha.glasgow@yahoo.com>
2022-03-02 12:33:24 -0500

    second

---
+++ script.js
@@ -0,0 +1,7 @@
+const section = document.querySelector('section');
+const areYouSmart = document.querySelector('span');
+const areYouLives = 7;
+
+
+//link text 
+areYouSmart.textContent = areYouLives;
